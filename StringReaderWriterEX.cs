a52efde81e68e2fa53924019e78849242e93a077
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-01 17:26:05 +0530

    Add project files.

---
+++ StringReaderWriterEX.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace StringReaderWriterDemo
+{
+    class StringReaderWriterEX
+    {
+        StringBuilder SB = new StringBuilder();
+        public StringReaderWriterEX()
+        {
+            Writer();
+
+            Reader();
+        }
+        static void Main(string[] args)
+        {
+            
+            //File.Create(@"D:\wizzie\DOT NET\StringReadWrite.txt");
+            //FileInfo f1 = new FileInfo(@"D:\wizzie\DOT NET\StringReadWrite.txt");
+            //Console.WriteLine(f1.Exists);
+            //Console.WriteLine(f1.Extension);
+            
+
+
+            
+        }
+        public void Writer()
+        {
+            StringWriter SW = new StringWriter();
+            Console.WriteLine("Welcome to String Writer Program");
+            SW.Write("Name: ");
+            String name = Console.ReadLine();
+
+            SW.WriteLine("NAME: " + name);
+            SW.Write("Contry: ");
+            String contry = Console.ReadLine();
+
+            SW.WriteLine("Contry: " + contry);
+            SW.Write("Age: ");
+            string age = Console.ReadLine();
+
+            SW.WriteLine("Age: " + age);
+            SW.WriteLine("Thank you!!!");
+            SW.WriteLine("Information Saved!");
+
+            SW.Flush();
+            SW.Close();
+        }
+        public void Reader()
+        {
+            StringReader SR = new StringReader(SB.ToString());
+            Console.WriteLine("Reading Profiel");
+
+            while (SR.Peek() > -1)
+            {
+                Console.WriteLine(SR.ReadLine());
+            }
+            Console.WriteLine("DATA READ COMPLETED!!!!");
+            SR.Close();
+        }
+    }
+}
